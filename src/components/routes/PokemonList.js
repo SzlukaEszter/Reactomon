9122b2753e150dee4d84b31f62bdf1a3b9453e87
Eszter Szluka <szluka.eszter@gmail.com>
2020-02-18 14:18:25 +0100

    'modify PokemonList.js to functional component'

--- src/components/routes/PokemonList.js
+++ src/components/routes/PokemonList.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import uuid from 'uuid';
 import PokemonCard from "../PokemonCard";
 
 
-class PokemonList extends Component {
+const PokemonList = props =>{
 
-  render(){
       return (
         <div style={{display : 'flex', flexFlow: 'row wrap'}}>
-        {this.props.pokemons.map((pokemon) =>
+        {props.pokemons.map((pokemon) =>
             (<PokemonCard key={uuid.v4()} pokemon = {pokemon}></PokemonCard> ))}
             </div>
             );
-  }
 }
   export default PokemonList;
