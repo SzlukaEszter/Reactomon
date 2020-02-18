96eb171e1f005117993323c0a9415670884fe855
Eszter Szluka <szluka.eszter@gmail.com>
2020-02-18 14:41:36 +0100

    'Modifying PokemonCard.js to functional component'

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 
-export class PokemonCard extends Component {
+const PokemonCard = props =>
+ {
 
-    render() {
-
-        return (
-            <div style={{
-                margin: '20px', 
-                padding: '30px', 
-                border: '3px solid orange',
-                borderRadius: '10px',
-                boxShadow: '5px 5px 3px grey',
-                backgroundColor: 'yellow',
-                color: 'blue'
-            }}>
-                <h2>{this.props.pokemon.name}</h2>
-                <Link style={{color: 'black'}} to={this.props.pokemon.url}>Details</Link> 
-            </div>
-        )
-    }
+    return (
+        <div style={{
+            margin: '20px', 
+            padding: '30px', 
+            border: '3px solid orange',
+            borderRadius: '10px',
+            boxShadow: '5px 5px 3px grey',
+            backgroundColor: 'yellow',
+            color: 'blue'
+        }}>
+            <h2>{props.pokemon.name}</h2>
+            <Link style={{color: 'black'}} to={props.pokemon.url}>Details</Link> 
+        </div>
+    )
+   
 }
 
 export default PokemonCard
