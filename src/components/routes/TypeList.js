7afe69b3636b2894937049bfa5b005ba7ac1ac9c
Eszter Szluka <szluka.eszter@gmail.com>
2020-02-18 14:21:30 +0100

    'Modifying TypeList.js to functional componet

--- src/components/routes/TypeList.js
+++ src/components/routes/TypeList.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PokemonCard from '../PokemonCard';
 import uuid from 'uuid';
 
-export class TypeList extends Component {
-    render() {
-        return (
-            <div>
-                <div style={{display : 'flex', flexFlow: 'row wrap'}}>
-        {this.props.types.map((type) =>
-            (<PokemonCard key={uuid.v4()} pokemon = {type}></PokemonCard> ))}
+const TypeList = props =>{
+    return (
+        <div>
+            <div style={{display : 'flex', flexFlow: 'row wrap'}}>
+                {props.types.map((type) =>
+                (<PokemonCard key={uuid.v4()} pokemon = {type}></PokemonCard> ))}
             </div>
-            </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default TypeList
