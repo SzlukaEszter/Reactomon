f664c712245868e0bd67f04e8df796951fd71c34
Eszter Szluka <szluka.eszter@gmail.com>
2020-02-21 17:30:39 +0100

    'remove unused code and import'

--- src/App.js
+++ src/App.js
@@ -5,24 +5,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/routes/Home";
 import PokemonList from "./components/routes/PokemonList";
 import TypeList from "./components/routes/TypeList";
-import { useEffect, useState } from "react";
-import axios from "axios";
 import { useHttp } from "./hooks/Http";
 
 const App = props => {
-  /*const [pokemons, setPokemons] = useState([]);
-
-  const [types, setTypes] = useState([]);
-
-
-  const pokemonHandler = (res) => {
-    setPokemons(res.data.results);
-  }
-
-  const typeHandler = (res) => {
-    setTypes(res.data.results);
-  }
-*/
+  
   const [isLoadingPokemons, pokemons] = useHttp(
     "https://pokeapi.co/api/v2/pokemon",
     []
@@ -30,19 +16,7 @@ const App = props => {
   const [isLoadingTypes, types] = useHttp("https://pokeapi.co/api/v2/type", []);
   console.log(pokemons);
   console.log(types);
-  /*
- useEffect(() => {
-    axios.get('https://pokeapi.co/api/v2/pokemon'
-      ).then(res => pokemonHandler(res))
-  }, []);
-
-  useEffect(() => {
-    axios.get('https://pokeapi.co/api/v2/type')
-    .then(res => typeHandler(res))
-  }, []);
-
-*/
-
+  
   const getListStyle = () => {
     return { display: "flex" };
   };
